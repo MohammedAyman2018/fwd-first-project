689bbd5d79d1667ea56f3ec68fa95778f00c3db5
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-28 23:12:43 +0200

    feat: add validation if heght or width are 0.

--- src/index.ts
+++ src/index.ts
@@ -15,29 +15,25 @@ app.get(
     // Validate all queries are provided.
     if (!fileName) return res.status(400).json('please provide fileName query');
     if (!width) return res.status(400).json('please provide width query');
-    if (isNaN(Number(width)) || Number(width) < 0)
+    if (isNaN(Number(width)) || Number(width) <= 0)
       return res.status(400).json('please provide valid width query');
     if (!height) return res.status(400).json('please provide height query');
-    if (isNaN(Number(height)) || Number(height) < 0)
+    if (isNaN(Number(height)) || Number(height) <= 0)
       return res.status(400).json('please provide valid height query');
 
     // Get the required image.
     const imagesPath = path.join(__dirname, '..', 'images');
     const exactImagePath = path.join(imagesPath, `${fileName}.jpg`);
     try {
-      fs.readFile(
-        exactImagePath,
-        async (
-          err: Error | null,
-          data: Buffer
-        ): Promise<void | express.Response<string, Record<string, File>>> => {
-          if (err) return res.status(500).json("Can't find this image.");
-          await createThump(data, `${fileName}`, Number(width), Number(height));
+      fs.readFile(exactImagePath, async (err: Error | null, data: Buffer) => {
+        if (err) return res.status(500).json("Can't find this image.");
+        await createThump(data, `${fileName}`, Number(width), Number(height));
+        process.nextTick(() => {
           return res.sendFile(
             path.join(__dirname, '..', 'thump', `${fileName}.jpg`)
           );
-        }
-      );
+        });
+      });
     } catch (error) {
       return res.status(500).json(`Something wrong happened ${error}`);
     }
